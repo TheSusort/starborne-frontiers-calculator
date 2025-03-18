014eb626e6795aa71ab1197d7a22740bb5a64aa8
TheSusort <kennethsusort@gmail.com>
2025-03-18 20:01:26 +0100

    [FEATURE] dps and def calcs

--- src/pages/DefenseCalculatorPage.tsx
+++ src/pages/DefenseCalculatorPage.tsx
@@ -128,7 +128,7 @@ const DefenseCalculatorPage: React.FC = () => {
             }}
         >
             <div className="space-y-6">
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                     {configs.map((config) => (
                         <div
                             key={config.id}
