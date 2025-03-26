09b4f86ae441a63adf24f4ef5d6afe3e4e2211d7
TheSusort <kennethsusort@gmail.com>
2025-03-26 12:16:49 +0100

    [FEATURE] damage deconstruction to find enemy defense

--- src/constants/changelog.ts
+++ src/constants/changelog.ts
@@ -1,8 +1,13 @@
 import { ChangelogEntry } from '../types/changelog';
 
-export const CURRENT_VERSION = '0.23.0';
+export const CURRENT_VERSION = '0.24.0';
 
 export const CHANGELOG: ChangelogEntry[] = [
+    {
+        version: '0.24.0',
+        date: '2025-03-26',
+        changes: ['Added healing calculator', 'Added hit deconstruction calculator'],
+    },
     {
         version: '0.23.0',
         date: '2025-03-18',
