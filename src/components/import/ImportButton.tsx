213633e8cbab29ee290a991a7018ed5a4879972e
TheSusort <kennethsusort@gmail.com>
2025-07-10 21:06:08 +0200

    [FEATURE] add json diff calculator

--- src/components/import/ImportButton.tsx
+++ src/components/import/ImportButton.tsx
@@ -152,7 +152,7 @@ export const ImportButton: React.FC<{ className?: string }> = ({ className = '' 
         <div>
             <Checkbox
                 id="share-data"
-                className=""
+                className="hidden"
                 label="Upload to cubedweb"
                 helpLabel="Check this if you want to upload your data to frontiers.cubedweb.net aswell, a tool to create shareable hangars."
                 checked={shareData}
