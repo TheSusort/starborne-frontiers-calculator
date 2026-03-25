4f78c2c2543fa24b8bb578cd14a18c79a7b08d02
TheSusort <kennethsusort@gmail.com>
2026-03-25 14:18:10 +0100

    feat: add more healing modifier steps in healing calcuator

--- src/pages/calculators/HealingCalculatorPage.tsx
+++ src/pages/calculators/HealingCalculatorPage.tsx
@@ -55,8 +55,11 @@ const calculateHealing = (config: HealerConfig) => {
 // Heal modifier options
 const healModifierOptions = [
     { value: '0', label: '0%' },
+    { value: '10', label: '10%' },
     { value: '20', label: '20%' },
+    { value: '30', label: '30%' },
     { value: '40', label: '40%' },
+    { value: '50', label: '50%' },
     { value: '60', label: '60%' },
 ];
 
